e96c81354659561bc233cbde08c224c39244ab66
AdrianRazor <adrianrazor@ukr.net>
2021-11-26 15:10:14 +0200

    homework
    added features filter

--- js/filters.js
+++ js/filters.js
@@ -5,7 +5,6 @@ import { setPins, removePins } from "./pins.js";
 function filterData (rawData, config) {
     let filteredData = rawData;
 
-
     if(config.type !== ANY) {
         filteredData = filteredData.filter(function (mock) {
             if (mock.offer.type === config.type) {
@@ -13,9 +12,39 @@ function filterData (rawData, config) {
             } else {
                 return false
             }
-             
         })
     }
+
+    if(config.price !== ANY) {
+        filteredData = filteredData.filter(function (mock) {
+            if (mock.offer.price === config.price) {
+                return true
+            } else {
+                return false
+            }
+        })
+        console.log(filteredData);
+    }
+
+    // if(config.rooms !== ANY) {
+    //     filteredData = filteredData.filter(function (mock) {
+    //         if (mock.offer.rooms === config.rooms) {
+    //             return true
+    //         } else {
+    //             return false
+    //         }
+    //     })
+    // }
+
+    // if(config.guests !== ANY) {
+    //     filteredData = filteredData.filter(function (mock) {
+    //         if (mock.offer.guests === config.guests) {
+    //             return true
+    //         } else {
+    //             return false
+    //         }
+    //     })
+    // }
 
     if(config.wifi) {
         filteredData = filteredData.filter(function (mock) {
@@ -24,12 +53,62 @@ function filterData (rawData, config) {
             } else {
                 return false
             }
-             
+        })
+    }
+
+    if(config.dishwasher) {
+        filteredData = filteredData.filter(function (mock) {
+            if (mock.offer.features.includes('dishwasher')) {
+                return true
+            } else {
+                return false
+            }
+        })
+    }
+    
+    if(config.parking) {
+        filteredData = filteredData.filter(function (mock) {
+            if (mock.offer.features.includes('parking')) {
+                return true
+            } else {
+                return false
+            }
+        })
+    }
+    
+    if(config.washer) {
+        filteredData = filteredData.filter(function (mock) {
+            if (mock.offer.features.includes('washer')) {
+                return true
+            } else {
+                return false
+            }
+        })
+    }
+    
+    if(config.elevator) {
+        filteredData = filteredData.filter(function (mock) {
+            if (mock.offer.features.includes('elevator')) {
+                return true
+            } else {
+                return false
+            }
+        })
+    }
+    
+    if(config.conditioner) {
+        filteredData = filteredData.filter(function (mock) {
+            if (mock.offer.features.includes('conditioner')) {
+                return true
+            } else {
+                return false
+            }
         })
     }
 
     return filteredData
 };
+
 
 export default function getFilterValues (data) {
     const form = document.querySelector('.map__filters')
@@ -38,7 +117,15 @@ export default function getFilterValues (data) {
     form.addEventListener('change', function () {
         filteredData = filterData(data, {
             type: getNodeValue('#housingtype'),
-            wifi: getNodeValue('#filter-wifi', 'checked')
+            price: getNodeValue('#housingprice'),
+            // rooms: getNodeValue('#housing-rooms'),
+            // guests: getNodeValue('#housing-guests'),
+            wifi: getNodeValue('#filter-wifi', 'checked'),
+            dishwasher: getNodeValue('#filter-dishwasher', 'checked'),
+            parking: getNodeValue('#filter-parking', 'checked'),
+            washer: getNodeValue('#filter-washer', 'checked'),
+            elevator: getNodeValue('#filter-elevator', 'checked'),
+            conditioner: getNodeValue('#filter-conditioner', 'checked'),
         })
         removePins()
         setPins(filteredData)
